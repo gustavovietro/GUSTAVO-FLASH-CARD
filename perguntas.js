660cfb5e4c90f1f1a1f281ee67d0d0fb26cb5c85
gustavovietro <143208792+gustavovietro@users.noreply.github.com>
2024-11-18 12:10:00 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'futebol',
+'Quem ganhou a última copa?',
+    'Argentina'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem ganhou a última Bola de Ouro?',
+    'Rodri'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem foi o campeão da copa de 1978?',
+    'Argentina'
+)
+
+criaCartao(
+'Futebol',
+'1 jogador que já ganhou 6 libertadores?',
+'Francisco Sá.'
+)
+
+criaCartao(
+    'Futebol',
+    'Goleiro que já ganhou Bola de Ouro',
+    'Yashin'
+    )
+
+criaCartao(
+        'Futebol',
+        'Qual páis ganhou a copa de 1954?',
+        'Alemanha'
+        )
+
+criaCartao(
+            'Futebol',
+            'Que ano Pelé nasceu?',
+            '23 de Outubro de 1940'
+            )
+
+criaCartao(
+                'Futebol',
+                'Quem ganhou a eurocopa de 2012?',
+                'Espanha'
+                )
